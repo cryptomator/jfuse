a32b70024854751cf6246a6b2f349b03f2045fda
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2022-01-18 08:54:34 +0100

    don't run benchmark test during CI

--- jfuse-tests/src/test/java/org/cryptomator/jfuse/tests/BenchmarkTest.java
+++ jfuse-tests/src/test/java/org/cryptomator/jfuse/tests/BenchmarkTest.java
@@ -1,11 +1,17 @@
 package org.cryptomator.jfuse.tests;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,8 +21,10 @@ import java.util.concurrent.TimeUnit;
 
 public class BenchmarkTest {
 
-	public static void main(String[] args) throws IOException {
-		org.openjdk.jmh.Main.main(args);
+	@Disabled("only on demand")
+	@Test
+	public void runBenchmarks() throws IOException {
+		org.openjdk.jmh.Main.main(new String[0]);
 	}
 
 	@Benchmark
