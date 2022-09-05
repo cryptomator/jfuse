3fe1bb626d7cafac2dd9c9cd6dc21681f70c2f1d
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2022-09-05 13:28:33 +0200

    fixed test

--- jfuse-linux-aarch64/src/main/java/org/cryptomator/jfuse/linux/aarch64/FuseArgs.java
+++ jfuse-linux-aarch64/src/main/java/org/cryptomator/jfuse/linux/aarch64/FuseArgs.java
@@ -20,7 +20,7 @@ record FuseArgs(MemorySegment args, MemorySegment cmdLineOpts) {
 		}
 		sb.append("mountPoint = ").append(mountPoint().getUtf8String(0));
 		sb.append("debug = ").append(fuse_cmdline_opts.debug$get(cmdLineOpts));
-		sb.append("singlethreaded = ").append(fuse_cmdline_opts.singlethread$get(cmdLineOpts));
+		sb.append("singlethreaded = ").append(!multithreaded());
 		return sb.toString();
 	}
 
