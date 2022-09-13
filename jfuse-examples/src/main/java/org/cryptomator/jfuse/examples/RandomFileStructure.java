35d89a13d60fc46937a48a44656f6f76b3968e7c
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2022-09-13 15:28:30 +0200

    add a RandomFileSystem for testing large directory trees

---
+++ jfuse-examples/src/main/java/org/cryptomator/jfuse/examples/RandomFileStructure.java
@@ -0,0 +1,80 @@
+package org.cryptomator.jfuse.examples;
+
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+
+/**
+ * A pseudo-randomized readonly file tree.
+ */
+public class RandomFileStructure {
+
+	private static final SortedMap<String, Node> NO_CHILDREN = Collections.unmodifiableSortedMap(new TreeMap<>());
+
+	private final Node root;
+
+	private RandomFileStructure(Node root) {
+		this.root = root;
+	}
+
+	public static RandomFileStructure init(long seed, int maxChildren) {
+		RandomGenerator rng = RandomGeneratorFactory.getDefault().create(seed);
+		var children = new TreeMap<String, Node>();
+		for (int i = 0; i < maxChildren; i++) {
+			var child = genNode(rng, maxChildren / 2);
+			children.put(child.name, child);
+		}
+		var root = new Node("",true, Instant.EPOCH, 0, children);
+		return new RandomFileStructure(root);
+	}
+
+	private static Node genNode(RandomGenerator rng, int maxChildren) {
+		var bytes = new byte[15];
+		rng.nextBytes(bytes);
+		var name = Base64.getUrlEncoder().encodeToString(bytes);
+		var isDir = rng.nextInt(10) < 2; // 20% directories
+		var lastModified = Instant.ofEpochSecond(rng.nextLong(0, 32503676399L));
+		if (isDir) {
+			var children = new TreeMap<String, Node>();
+			var nChildren = rng.nextInt(maxChildren);
+			for (int i = 0; i < nChildren; i++) {
+				var child = genNode(rng, maxChildren / 2);
+				children.put(child.name, child);
+			}
+			return new Node(name, isDir, lastModified, 0, children);
+		} else {
+			var size = isDir ? 0 : rng.nextInt(0, 500_000_000);
+			return new Node(name, isDir, lastModified, size, NO_CHILDREN);
+		}
+	}
+
+	public Node getNode(String absPath) {
+		assert absPath.indexOf('/') == 0;
+		var subPath = absPath.substring(1);
+		return getNode(root, subPath);
+	}
+
+	private Node getNode(Node base, String relPath) {
+		int idx = relPath.indexOf('/');
+		var current = idx == -1 ? relPath : relPath.substring(0, idx);
+		var remaining = idx == -1 ? "" : relPath.substring(idx + 1);
+		if (current.isEmpty()) {
+			return base;
+		} else {
+			var child = base.children.get(current);
+			if (child == null) {
+				return null;
+			} else {
+				return getNode(child, remaining);
+			}
+		}
+	}
+
+	public record Node (String name, boolean isDir, Instant lastModified, long size, SortedMap<String, Node> children) {
+	}
+
+}
---
+++ jfuse-examples/src/main/java/org/cryptomator/jfuse/examples/RandomFileSystem.java
@@ -0,0 +1,135 @@
+package org.cryptomator.jfuse.examples;
+
+import org.cryptomator.jfuse.api.DirFiller;
+import org.cryptomator.jfuse.api.Errno;
+import org.cryptomator.jfuse.api.FileInfo;
+import org.cryptomator.jfuse.api.Fuse;
+import org.cryptomator.jfuse.api.FuseConnInfo;
+import org.cryptomator.jfuse.api.FuseOperations;
+import org.cryptomator.jfuse.api.MountFailedException;
+import org.cryptomator.jfuse.api.Stat;
+import org.cryptomator.jfuse.api.Statvfs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.concurrent.TimeoutException;
+
+@SuppressWarnings("OctalInteger")
+public class RandomFileSystem implements FuseOperations {
+
+	private static final int S_IFDIR = 0040000;
+	private static final int S_IFREG = 0100000;
+	private static final Logger LOG = LoggerFactory.getLogger(HelloWorldFileSystem.class);
+
+	private final Errno errno;
+	private final RandomFileStructure rfs;
+
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			LOG.error("Invalid number of arguments. Expected {mountPoint}.");
+		}
+		Path mountPoint = Path.of(args[0]);
+		var builder = Fuse.builder();
+		var fuseOps = new RandomFileSystem(builder.errno());
+		try (var fuse = builder.build(fuseOps)) {
+			LOG.info("Mounting at {}...", mountPoint);
+			fuse.mount("jfuse", mountPoint, "-s");
+			LOG.info("Mounted to {}.", mountPoint);
+			LOG.info("Enter a anything to unmount...");
+			System.in.read();
+		} catch (MountFailedException | TimeoutException e) {
+			LOG.error("Un/Mounting failed. ", e);
+			System.exit(1);
+		} catch (IOException e) {
+			LOG.error("Failed to create mirror", e);
+			System.exit(1);
+		}
+	}
+
+	public RandomFileSystem(Errno errno) {
+		this.errno = errno;
+		this.rfs = RandomFileStructure.init(42L, 100);
+	}
+
+	@Override
+	public Errno errno() {
+		return errno;
+	}
+
+	@Override
+	public Set<Operation> supportedOperations() {
+		return EnumSet.of(Operation.GET_ATTR, Operation.OPEN_DIR, Operation.READ_DIR, Operation.RELEASE_DIR, Operation.STATFS);
+	}
+
+	@Override
+	public int getattr(String path, Stat stat, FileInfo fi) {
+		LOG.debug("getattr() {}", path);
+		var node = rfs.getNode(path);
+		if (node == null) {
+			return -errno.enoent();
+		} else if (node.isDir()) {
+			stat.setMode(S_IFDIR | 0755);
+			stat.setNLink((short) (2 + node.children().size()));
+			stat.mTime().set(node.lastModified());
+			return 0;
+		} else {
+			stat.setMode(S_IFREG | 0444);
+			stat.setNLink((short) 1);
+			stat.setSize(0);
+			stat.mTime().set(node.lastModified());
+			return 0;
+		}
+	}
+
+	@Override
+	public int opendir(String path, FileInfo fi) {
+		LOG.debug("opendir() {}", path);
+		return 0;
+	}
+
+	@Override
+	public int readdir(String path, DirFiller filler, long offset, FileInfo fi, Set<ReadDirFlags> flags) {
+		LOG.debug("readdir() {} {}", path, offset);
+		var node = rfs.getNode(path);
+		if (node == null) {
+			return -errno.enoent();
+		} else if (!node.isDir()) {
+			return -errno.enotdir();
+		} else {
+			try {
+				filler.fill(".", null);
+				filler.fill("..", null);
+				for (var name : node.children().keySet()) {
+					filler.fill(name, null);
+				}
+				return 0;
+			} catch (IOException e) {
+				return -errno.eio();
+			}
+		}
+	}
+
+	@Override
+	public int releasedir(String path, FileInfo fi) {
+		LOG.debug("releasedir() {}", path);
+		return 0;
+	}
+
+	@Override
+	public int statfs(String path, Statvfs statvfs) {
+		LOG.debug("statfs() {}", path);
+		statvfs.setNameMax(255);
+		statvfs.setBsize(4096);
+		statvfs.setBlocks(1000);
+		statvfs.setBfree(500);
+		statvfs.setBavail(500);
+		return 0;
+	}
+}
