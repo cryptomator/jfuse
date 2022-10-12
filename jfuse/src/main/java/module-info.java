22fe5a173bb8c4c5a7032ae6a4def10c1dca1157
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2022-10-12 16:40:44 +0200

    javadoc
    [ci skip]

--- jfuse/src/main/java/module-info.java
+++ jfuse/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 /**
- * Defines the API to create a FUSE file system in Java.
+ * This is just an aggregator module. Please refer to the API module.
  */
 module org.cryptomator.jfuse {
 	requires transitive org.cryptomator.jfuse.api;
+
 	requires org.cryptomator.jfuse.linux.aarch64;
 	requires org.cryptomator.jfuse.linux.amd64;
 	requires org.cryptomator.jfuse.mac;
