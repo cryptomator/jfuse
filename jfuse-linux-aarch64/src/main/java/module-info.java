6526eb4f85a0991131226bb55f87453f23f5dd40
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2022-10-12 13:40:55 +0200

    added JavaDoc
    [ci skip]

--- jfuse-linux-aarch64/src/main/java/module-info.java
+++ jfuse-linux-aarch64/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 import org.cryptomator.jfuse.api.FuseBuilder;
 import org.cryptomator.jfuse.linux.aarch64.LinuxFuseBuilder;
 
+/**
+ * @provides FuseBuilder FUSE builder for Linux (aarch64)
+ */
 @SuppressWarnings("JavaModuleNaming") // 64 is not a "version", see https://bugs.openjdk.java.net/browse/JDK-8264488
 module org.cryptomator.jfuse.linux.aarch64 {
 	requires static org.jetbrains.annotations;
