aa7c036e9fbef9749e6dec60740c37baf1063bed
Armin Schrenk <armin.schrenk@skymatic.de>
2023-08-23 10:12:25 +0200

    improve errno unit tests

--- jfuse-linux-aarch64/src/test/java/org/cryptomator/jfuse/linux/aarch64/LinuxErrnoTest.java
+++ jfuse-linux-aarch64/src/test/java/org/cryptomator/jfuse/linux/aarch64/LinuxErrnoTest.java
@@ -1,22 +1,32 @@
 package org.cryptomator.jfuse.linux.aarch64;
 
+import org.cryptomator.jfuse.api.Errno;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class LinuxErrnoTest {
 
-	@DisplayName("make sure method is not a stub")
+	@DisplayName("make sure no errno method is not a stub")
 	@ParameterizedTest(name = "{0}()")
-	@ValueSource(strings = {"enoent", "enosys", "enomem", "eacces", "eio", "erofs", "ebadf", "eexist", "enotdir", "eisdir", "enotempty", "einval"})
-	public void testErrnoIsNotZero(String methodName) throws ReflectiveOperationException {
+	@MethodSource("errnoNameProvider")
+	public void testErrnoIsImplemented(String methodName) throws ReflectiveOperationException {
 		var errno = new LinuxErrno();
 		var method = LinuxErrno.class.getMethod(methodName);
 
 		int result = (int) method.invoke(errno);
 
 		Assertions.assertNotEquals(0, result);
+	}
+
+	static Stream<String> errnoNameProvider() {
+		return Arrays.stream(Errno.class.getDeclaredMethods()).map(Method::getName);
 	}
 
 }
