e8822fa93d874faf17d98a725f6029ff523aeb3a
Armin Schrenk <armin.schrenk@skymatic.de>
2023-08-30 12:01:20 +0200

    improve errno unit test for mac

--- jfuse-mac/src/test/java/org/cryptomator/jfuse/mac/MacErrnoTest.java
+++ jfuse-mac/src/test/java/org/cryptomator/jfuse/mac/MacErrnoTest.java
@@ -1,22 +1,31 @@
 package org.cryptomator.jfuse.mac;
 
+import org.cryptomator.jfuse.api.Errno;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class MacErrnoTest {
 
-	@DisplayName("make sure method is not a stub")
+	@DisplayName("make sure errno method is not a stub")
 	@ParameterizedTest(name = "{0}()")
-	@ValueSource(strings = {"enoent", "enosys", "enomem", "eacces", "eio", "erofs", "ebadf", "eexist", "enotdir", "eisdir", "enotempty", "einval"})
-	public void testErrnoIsNotZero(String methodName) throws ReflectiveOperationException {
+	@MethodSource("errnoNameProvider")
+	public void testErrnoIsImplemented(String methodName) throws ReflectiveOperationException {
 		var errno = new MacErrno();
 		var method = MacErrno.class.getMethod(methodName);
 
 		int result = (int) method.invoke(errno);
 
 		Assertions.assertNotEquals(0, result);
+	}
+
+	static Stream<String> errnoNameProvider() {
+		return Arrays.stream(Errno.class.getDeclaredMethods()).map(Method::getName);
 	}
 
 }
