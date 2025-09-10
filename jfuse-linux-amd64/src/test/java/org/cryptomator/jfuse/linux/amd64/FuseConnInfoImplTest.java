545d432367e558efaff2e7ce4f79870a2a7b055a
Armin Schrenk <armin.schrenk@skymatic.de>
2025-09-10 14:03:03 +0200

    extend test

--- jfuse-linux-amd64/src/test/java/org/cryptomator/jfuse/linux/amd64/FuseConnInfoImplTest.java
+++ jfuse-linux-amd64/src/test/java/org/cryptomator/jfuse/linux/amd64/FuseConnInfoImplTest.java
@@ -5,6 +5,7 @@ import org.cryptomator.jfuse.linux.amd64.extr.fuse3.fuse_conn_info;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Named;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,67 +18,143 @@ import java.util.stream.Stream;
 
 public class FuseConnInfoImplTest {
 
-	@DisplayName("test getters")
-	@ParameterizedTest(name = "{1}")
-	@MethodSource
-	public void testGetters(SetInMemorySegment setter, GetInConnInfo getter) {
-		try (var arena = Arena.ofConfined()) {
-			var segment = fuse_conn_info.allocate(arena);
-			var connInfo = new FuseConnInfoImpl(segment);
+	@Nested
+	public class IntFieldTests {
 
-			setter.accept(segment, 42);
+		@DisplayName("test getters")
+		@ParameterizedTest(name = "{1}")
+		@MethodSource
+		void testGetters(SetInMemorySegment setter, GetInConnInfo getter) {
+			try (var arena = Arena.ofConfined()) {
+				var segment = fuse_conn_info.allocate(arena);
+				var connInfo = new FuseConnInfoImpl(segment);
 
-			Assertions.assertEquals(42, getter.apply(connInfo));
+				setter.accept(segment, 42);
+
+				Assertions.assertEquals(42, getter.apply(connInfo));
+			}
+		}
+
+		public static Stream<Arguments> testGetters() {
+			return Stream.of(
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::proto_major, Named.of("protoMajor()", (GetInConnInfo) FuseConnInfo::protoMajor)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::proto_minor, Named.of("protoMinor()", (GetInConnInfo) FuseConnInfo::protoMinor)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::capable, Named.of("capable()", (GetInConnInfo) FuseConnInfo::capable)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::max_write, Named.of("maxWrite()", (GetInConnInfo) FuseConnInfo::maxWrite)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::max_read, Named.of("maxRead()", (GetInConnInfo) FuseConnInfo::maxRead)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::max_readahead, Named.of("maxReadahead()", (GetInConnInfo) FuseConnInfo::maxReadahead)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::max_background, Named.of("maxBackground()", (GetInConnInfo) FuseConnInfo::maxBackground)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::congestion_threshold, Named.of("congestionThreshold()", (GetInConnInfo) FuseConnInfo::congestionThreshold)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::time_gran, Named.of("timeGran()", (GetInConnInfo) FuseConnInfo::timeGran)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::want, Named.of("want()", (GetInConnInfo) FuseConnInfo::want))
+			);
+		}
+
+		interface SetInMemorySegment extends GenericSetInMemorySegment<Integer> {
+		}
+
+		interface GetInConnInfo extends GenericGetInConnInfo<Integer> {
+		}
+
+		@DisplayName("test setters")
+		@ParameterizedTest(name = "{0}")
+		@MethodSource
+		void testSetters(SetInConnInfo setter, GetInMemorySegment getter) {
+			try (var arena = Arena.ofConfined()) {
+				var segment = fuse_conn_info.allocate(arena);
+				var connInfo = new FuseConnInfoImpl(segment);
+
+				setter.accept(connInfo, 42);
+
+				Assertions.assertEquals(42, getter.apply(segment));
+			}
+		}
+
+		public static Stream<Arguments> testSetters() {
+			return Stream.of(
+					Arguments.arguments(Named.of("setWant()", (SetInConnInfo) FuseConnInfo::setWant), (GetInMemorySegment) fuse_conn_info::want),
+					Arguments.arguments(Named.of("setMaxWrite()", (SetInConnInfo) FuseConnInfo::setMaxWrite), (GetInMemorySegment) fuse_conn_info::max_write),
+					Arguments.arguments(Named.of("setMaxRead()", (SetInConnInfo) FuseConnInfo::setMaxRead), (GetInMemorySegment) fuse_conn_info::max_read),
+					Arguments.arguments(Named.of("setMaxReadahead()", (SetInConnInfo) FuseConnInfo::setMaxReadahead), (GetInMemorySegment) fuse_conn_info::max_readahead),
+					Arguments.arguments(Named.of("setMaxBackground()", (SetInConnInfo) FuseConnInfo::setMaxBackground), (GetInMemorySegment) fuse_conn_info::max_background),
+					Arguments.arguments(Named.of("setCongestionThreshold()", (SetInConnInfo) FuseConnInfo::setCongestionThreshold), (GetInMemorySegment) fuse_conn_info::congestion_threshold),
+					Arguments.arguments(Named.of("setTimeGran()", (SetInConnInfo) FuseConnInfo::setTimeGran), (GetInMemorySegment) fuse_conn_info::time_gran)
+			);
+		}
+
+		interface SetInConnInfo extends GenericSetInConnInfo<Integer> {
+		}
+
+		interface GetInMemorySegment extends GenericGetInMemorySegment<Integer> {
 		}
 	}
 
-	public static Stream<Arguments> testGetters() {
-		return Stream.of(
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::proto_major, Named.of("protoMajor()", (GetInConnInfo) FuseConnInfo::protoMajor)),
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::proto_minor, Named.of("protoMinor()", (GetInConnInfo) FuseConnInfo::protoMinor)),
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::capable, Named.of("capable()", (GetInConnInfo) FuseConnInfo::capable)),
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::max_write, Named.of("maxWrite()", (GetInConnInfo) FuseConnInfo::maxWrite)),
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::max_read, Named.of("maxRead()", (GetInConnInfo) FuseConnInfo::maxRead)),
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::max_readahead, Named.of("maxReadahead()", (GetInConnInfo) FuseConnInfo::maxReadahead)),
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::max_background, Named.of("maxBackground()", (GetInConnInfo) FuseConnInfo::maxBackground)),
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::congestion_threshold, Named.of("congestionThreshold()", (GetInConnInfo) FuseConnInfo::congestionThreshold)),
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::time_gran, Named.of("timeGran()", (GetInConnInfo) FuseConnInfo::timeGran)),
-				Arguments.arguments((SetInMemorySegment) fuse_conn_info::want, Named.of("want()", (GetInConnInfo) FuseConnInfo::want))
-		);
-	}
+	@Nested
+	public class LongFieldTest {
 
-	private interface SetInMemorySegment extends BiConsumer<MemorySegment, Integer> {}
+		@DisplayName("test long getters")
+		@ParameterizedTest(name = "{1}")
+		@MethodSource
+		void testGetters(SetInMemorySegment setter, GetInConnInfo getter) {
+			try (var arena = Arena.ofConfined()) {
+				var segment = fuse_conn_info.allocate(arena);
+				var connInfo = new FuseConnInfoImpl(segment);
 
-	private interface GetInConnInfo extends Function<FuseConnInfo, Integer> {}
+				setter.accept(segment, 42L);
 
-	@DisplayName("test setters")
-	@ParameterizedTest(name = "{0}")
-	@MethodSource
-	public void testSetters(SetInConnInfo setter, GetInMemorySegment getter) {
-		try (var arena = Arena.ofConfined()) {
-			var segment = fuse_conn_info.allocate(arena);
-			var connInfo = new FuseConnInfoImpl(segment);
+				Assertions.assertEquals(42L, getter.apply(connInfo));
+			}
+		}
 
-			setter.accept(connInfo, 42);
+		public static Stream<Arguments> testGetters() {
+			return Stream.of(
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::want_ext, Named.of("wantExt()", (GetInConnInfo) FuseConnInfo::wantExt)),
+					Arguments.arguments((SetInMemorySegment) fuse_conn_info::capable_ext, Named.of("capable()", (GetInConnInfo) FuseConnInfo::capableExt))
+			);
+		}
 
-			Assertions.assertEquals(42, getter.apply(segment));
+		interface SetInMemorySegment extends GenericSetInMemorySegment<Long> {
+		}
+
+		interface GetInConnInfo extends GenericGetInConnInfo<Long> {
+		}
+
+		@DisplayName("test setters")
+		@ParameterizedTest(name = "{0}")
+		@MethodSource
+		void testSetters(SetInConnInfo setter, GetInMemorySegment getter) {
+			try (var arena = Arena.ofConfined()) {
+				var segment = fuse_conn_info.allocate(arena);
+				var connInfo = new FuseConnInfoImpl(segment);
+
+				setter.accept(connInfo, 42L);
+
+				Assertions.assertEquals(42L, getter.apply(segment));
+			}
+		}
+
+		public static Stream<Arguments> testSetters() {
+			return Stream.of(
+					Arguments.arguments(Named.of("setWantExt()", (SetInConnInfo) FuseConnInfo::setWantExt), (GetInMemorySegment) fuse_conn_info::want_ext)
+			);
+		}
+
+		interface SetInConnInfo extends GenericSetInConnInfo<Long> {
+		}
+
+		interface GetInMemorySegment extends GenericGetInMemorySegment<Long> {
 		}
 	}
 
-	public static Stream<Arguments> testSetters() {
-		return Stream.of(
-				Arguments.arguments(Named.of("setWant()", (SetInConnInfo) FuseConnInfo::setWant), (GetInMemorySegment) fuse_conn_info::want),
-				Arguments.arguments(Named.of("setMaxWrite()", (SetInConnInfo) FuseConnInfo::setMaxWrite), (GetInMemorySegment) fuse_conn_info::max_write),
-				Arguments.arguments(Named.of("setMaxRead()", (SetInConnInfo) FuseConnInfo::setMaxRead), (GetInMemorySegment) fuse_conn_info::max_read),
-				Arguments.arguments(Named.of("setMaxReadahead()", (SetInConnInfo) FuseConnInfo::setMaxReadahead), (GetInMemorySegment) fuse_conn_info::max_readahead),
-				Arguments.arguments(Named.of("setMaxBackground()", (SetInConnInfo) FuseConnInfo::setMaxBackground), (GetInMemorySegment) fuse_conn_info::max_background),
-				Arguments.arguments(Named.of("setCongestionThreshold()", (SetInConnInfo) FuseConnInfo::setCongestionThreshold), (GetInMemorySegment) fuse_conn_info::congestion_threshold),
-				Arguments.arguments(Named.of("setTimeGran()", (SetInConnInfo) FuseConnInfo::setTimeGran), (GetInMemorySegment) fuse_conn_info::time_gran)
-		);
+	private interface GenericSetInMemorySegment<E extends Number> extends BiConsumer<MemorySegment, E> {
 	}
 
-	private interface SetInConnInfo extends BiConsumer<FuseConnInfo, Integer> {}
+	private interface GenericGetInConnInfo<E extends Number> extends Function<FuseConnInfo, E> {
+	}
 
-	private interface GetInMemorySegment extends Function<MemorySegment, Integer> {}
+	private interface GenericSetInConnInfo<E extends Number> extends BiConsumer<FuseConnInfo, E> {
+	}
 
+	private interface GenericGetInMemorySegment<E extends Number> extends Function<MemorySegment, E> {
+	}
 }
