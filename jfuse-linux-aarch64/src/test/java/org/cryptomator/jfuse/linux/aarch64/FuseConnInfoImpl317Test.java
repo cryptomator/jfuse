c1db90b97c53b67d40fbce7e6ec132bd50f8b47f
Armin Schrenk <armin.schrenk@skymatic.de>
2026-01-27 16:41:30 +0100

    add tests for fuseConfInfoImpl317

--- jfuse-linux-aarch64/src/test/java/org/cryptomator/jfuse/linux/aarch64/FuseConnInfoImpl317Test.java
+++ jfuse-linux-aarch64/src/test/java/org/cryptomator/jfuse/linux/aarch64/FuseConnInfoImpl317Test.java
@@ -5,9 +5,12 @@ import org.cryptomator.jfuse.linux.aarch64.extr.fuse3.fuse_conn_info;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Named;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Answers;
+import org.mockito.Mockito;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
@@ -15,7 +18,31 @@ import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+
 public class FuseConnInfoImpl317Test {
+
+	@DisplayName("XFeatureFlag methods are delegating to FuseFunctions")
+	@Test
+	void testXFeatureFlagMethods() {
+		try (var arena = Arena.ofConfined();
+			 var fuseFunctionsMock = Mockito.mockStatic(FuseFunctions.class)) {
+			var segment = fuse_conn_info.allocate(arena);
+			var connInfo = new FuseConnInfoImpl317(segment);
+			fuseFunctionsMock.when(() -> FuseFunctions.fuse_set_feature_flag(any(), anyLong())).thenReturn(true);
+			fuseFunctionsMock.when(() -> FuseFunctions.fuse_unset_feature_flag(any(), anyLong())).then(Answers.RETURNS_DEFAULTS);
+			fuseFunctionsMock.when(() -> FuseFunctions.fuse_get_feature_flag(any(), anyLong())).thenReturn(true);
+
+			Assertions.assertDoesNotThrow(() -> connInfo.setFeatureFlag(3003L));
+			Assertions.assertDoesNotThrow(() -> connInfo.getFeatureFlag(3003L));
+			Assertions.assertDoesNotThrow(() -> connInfo.unsetFeatureFlag(3003L));
+
+			fuseFunctionsMock.verify(() -> FuseFunctions.fuse_set_feature_flag(segment, 3003L));
+			fuseFunctionsMock.verify(() -> FuseFunctions.fuse_unset_feature_flag(segment, 3003L));
+			fuseFunctionsMock.verify(() -> FuseFunctions.fuse_get_feature_flag(segment, 3003L));
+		}
+	}
 
 	@DisplayName("test long getters")
 	@ParameterizedTest(name = "{1}")
